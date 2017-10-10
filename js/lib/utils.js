2ceb4d399919df74ec45bc6db34fdac6767e08bc
liangxiaoxin <651589762@qq.com>
2017-10-10 11:08:34 +0800

    数字千分位格式化

--- js/lib/utils.js
+++ js/lib/utils.js
@@ -59,3 +59,76 @@ export const simpleTips = (text, duration)=>{
     }
   }
 };
+/**
+ * 手机号加密
+ * @param phone
+ * @returns {string}
+ */
+export const encodePhone = (phone) => {
+  return phone.substr(0, 3) + '****' + phone.substr(7, 11);
+};
+/**
+ * 存cookie
+ * @param name
+ */
+export const setCookie = (name) => {
+  var exp = new Date();
+  exp.setTime(exp.getTime() + 30*60*1000);
+  document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+/**
+ * 读取cookie
+ * @param name
+ * @returns {null}
+ */
+export const setCookie = (name) => {
+  var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+
+  if(arr=document.cookie.match(reg))
+
+    return unescape(arr[2]);
+  else
+    return null;
+}
+/**
+ * 删除cookie
+ * @param name
+ */
+export const delCookie = (name) => {
+  var exp = new Date();
+  exp.setTime(exp.getTime() - 1);
+  var cval=getCookie(name);
+  if(cval!=null)
+    document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+};
+export const checkPhone = () => {
+  var phone = $(this).val();
+  if (!phone) {
+    showTips('手机号码不能为空');
+    $(this).val("").focus();
+    return false
+  }
+  if (!(/^1[34578]\d{9}$/.test(phone))) {
+    showTips('手机号码输入有误,请重新填写');
+    $(this).val("").focus();
+    return false
+  }
+}
+/**
+ * 提示
+ * @param text
+ * @param time
+ */
+export const showTips = (text,time=1500) => {
+  $('#disTips .text').html(text).parents('#disTips').fadeIn().delay(time).fadeOut(time);
+}
+/**
+ * 数字千分号
+ * @param num
+ * @returns {string}
+ */
+export const formatNumberRgx = (num) => {
+  var parts = num.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+};
